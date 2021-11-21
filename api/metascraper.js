ccaf40efaf8f08b6e0a262cb5831d858bda9a2bf
Nathan Gathright <nathan.gathright@gmail.com>
2021-11-21 11:05:52 -0600

    non-functional ideal code

---
+++ api/metascraper.js
@@ -0,0 +1,13 @@
+const got = require("got");
+const metascraper = require("metascraper")([
+  require("metascraper-iframe")(),
+  require("metascraper-url")(),
+]);
+
+export default async (req, res) => {
+    let targetUrl = new URL(req.query.url);
+    const { body: html, url } = await got(targetUrl);
+    const metadata = await metascraper({ html, url });
+    console.log(metadata);
+    res.json(metadata);
+}
