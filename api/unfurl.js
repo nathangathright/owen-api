7174d524d6a5765236dde2603c9da3d245c29931
Nathan Gathright <nathan.gathright@gmail.com>
2021-11-21 08:37:04 -0600

    Expand

--- api/unfurl.js
+++
@@ -1,7 +0,0 @@
-const unfurl = require("unfurl.js");
-
-module.exports = async (req, res) => {
-  let url = new URL(req.query.url);
-  url.protocol = "https:";
-  return res.send(await unfurl(url.href));
-};
