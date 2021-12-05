2018afcccc9febb9ea05dd0571c11830621c3be3
Nathan Gathright <nathan.gathright@gmail.com>
2021-12-05 11:46:09 -0600

    Slimmer syntax

--- api/slim.js
+++ api/slim.js
@@ -5,23 +5,23 @@ const { JSDOM } = jsdom;
 export default async (req, res) => {
   const targetURL = new URL(req.query.url)
   const response = await got(targetURL)
-  const dom = new JSDOM(response.body);
+  const { document } = (new JSDOM(response.body)).window;
 
   const getOembedUrl = () => 
-    dom.window.document.querySelector('link[type="application/json+oembed"]').getAttribute('href') || 
-    dom.window.document.querySelector('link[type="text/xml+oembed"]').getAttribute('href')
+    document.querySelector('link[type="application/json+oembed"]').getAttribute('href') || 
+    document.querySelector('link[type="text/xml+oembed"]').getAttribute('href') || ''
   const playerUrl = () => 
-    dom.window.document.querySelector('meta[name="twitter:player"]').getAttribute('content') || 
-    dom.window.document.querySelector('meta[property="twitter:player"]').getAttribute('content')
+    document.querySelector('meta[name="twitter:player"]').getAttribute('content') || 
+    document.querySelector('meta[property="twitter:player"]').getAttribute('content') || ''
   const playerStream = () => 
-    dom.window.document.querySelector('meta[name="twitter:player:stream"]').getAttribute('content') || 
-    dom.window.document.querySelector('meta[property="twitter:player:stream"]').getAttribute('content')
+    document.querySelector('meta[name="twitter:player:stream"]').getAttribute('content') || 
+    document.querySelector('meta[property="twitter:player:stream"]').getAttribute('content') || ''
   const playerWidth = () => 
-    dom.window.document.querySelector('meta[name="twitter:player:width"]').getAttribute('content') || 
-    dom.window.document.querySelector('meta[property="twitter:player:width"]').getAttribute('content')
+    document.querySelector('meta[name="twitter:player:width"]').getAttribute('content') || 
+    document.querySelector('meta[property="twitter:player:width"]').getAttribute('content') || ''
   const playerHeight = () => 
-    dom.window.document.querySelector('meta[name="twitter:player:height"]').getAttribute('content') || 
-    dom.window.document.querySelector('meta[property="twitter:player:height"]').getAttribute('content')
+    document.querySelector('meta[name="twitter:player:height"]').getAttribute('content') || 
+    document.querySelector('meta[property="twitter:player:height"]').getAttribute('content') || ''
 
   const twitterPlayer = {
     url: playerUrl(),
